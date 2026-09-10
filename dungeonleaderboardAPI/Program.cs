a3eb27c11b24853d3da5a1d5ca1008fb3648c6e9
MySTicIEj <wizzie.026x@gmail.com>
2026-09-10 18:20:55 +0200

    Update Program.cs

--- dungeonleaderboardAPI/Program.cs
+++ dungeonleaderboardAPI/Program.cs
@@ -1,9 +1,9 @@
 using DungeonLeaderboardAPI;
 
+var builder = WebApplication.CreateBuilder(args);
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<LeaderboardStore>();
 
